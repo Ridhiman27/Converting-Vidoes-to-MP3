f1341507cc894f0ec4a1f9a619965aabbc987256
Ridhiman27 <93005927+Ridhiman27@users.noreply.github.com>
2023-05-21 02:27:51 +0530

    Consumer Sevice

---
+++ server/converter/consumer.py
@@ -0,0 +1,23 @@
+import pika ,sys, os ,time
+from pymongo import MongoClient
+import gridfs
+from convert import to_mp3
+
+def main():
+    client = MongoClient("host.minikube.internal",27017)
+    db_videos = client.videos
+    db_mp3s = client.mp3s
+
+    fs_videos = gridfs.GridFS(db_videos)
+
+    fs_mp3s = gridfs.GridFS(db_mp3s)
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host="rabbitmq")
+    )
+
+    channel = connection.channel()
+
+    channel.basic_consume(
+
+    )
